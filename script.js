2a40b3078582e449c1fc5f03963d2edcf06bf69a
“noarrassam” <noor.s.rassam@gmail.com>
2022-04-09 20:23:44 -0400

    valuesToLocalStorage

--- script.js
+++ script.js
@@ -1,21 +1,42 @@
-let btn = document.getElementById("btn");
 let cities = document.getElementById("search");
-var para = document.getElementById("para");
-btn.addEventListener("click", function () {
+let arr = [];
+let btn = document.getElementById("btn");
+//btn.setAttribute("method", "GET");
+btn.addEventListener("click", setData);
+
+function setData() {
   if (cities.value.trim() != 0) {
+    // https://api.openweathermap.org/data/2.5/forecast?q=London&cnt=5&appid=eff04efa5709d18068cb132cce23a366&units=metric
     var weather =
-      "http://api.openweathermap.org/geo/1.0/direct?q=" +
+      "https://api.openweathermap.org/data/2.5/forecast?q=" +
       cities.value +
-      "&limit=5&appid=eff04efa5709d18068cb132cce23a366";
+      "&cnt=5&appid=eff04efa5709d18068cb132cce23a366&units=metric";
 
     $.ajax({
       url: weather,
       method: "GET",
     }).then(function (response) {
-      console.log(response);
-      // para.innerHTML = response[0].name;
-      para.innerHTML = response[0].state;
-      console.log(para);
+      arr.push(response.city["name"]);
+      localStorage.setItem("key", JSON.stringify(arr));
+      location.reload();
     });
   }
-});
+}
+
+function getData() {
+  if (localStorage.getItem("key") == null) {
+    arr = [];
+  } else {
+    arr = JSON.parse(localStorage.getItem("key"));
+    console.log(arr);
+    arr.forEach((item, index) => {
+      let li = document.createElement("li");
+      var text = document.createTextNode(item);
+      li.appendChild(text);
+      document.getElementById("ul").appendChild(li);
+      console.log(ul);
+    });
+  }
+}
+
+getData();
---
+++ test.js
@@ -0,0 +1,51 @@
+// console.log(response);
+// var htmlTemp = "°C";
+
+// prg.innerHTML = "Current Temp" + " " + res.list[0].main.temp + htmlTemp;
+
+// let img = document.createElement("img");
+// img.setAttribute("id", "wicon");
+// let icon = response.list[0].weather[0].icon;
+// var iconurl = "http://openweathermap.org/img/w/" + icon + ".png";
+// img.setAttribute("src", iconurl);
+
+// let dates = document.createElement("p");
+// var date = response.list[0].dt_txt;
+// var mydate = new Date(date);
+// console.log(mydate);
+// var day = [
+//   "Sunday",
+//   "Monday",
+//   "Tuesday",
+//   "Wednesday",
+//   "Thursday",
+//   "Friday",
+//   "Saturday",
+// ][mydate.getDay()];
+
+// var month = [
+//   "January",
+//   "February",
+//   "March",
+//   "April",
+//   "May",
+//   "June",
+//   "July",
+//   "August",
+//   "September",
+//   "October",
+//   "November",
+//   "December",
+// ][mydate.getMonth()];
+// var str =
+//   day +
+//   " " +
+//   month +
+//   " " +
+//   mydate.getDate() +
+//   ", " +
+//   mydate.getFullYear();
+// dates.innerHTML = str;
+// div.appendChild(img);
+
+// div.appendChild(dates);
