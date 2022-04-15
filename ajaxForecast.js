482bb7955ea55efb852d6c58c648319dcc99f3d9
“noarrassam” <noor.s.rassam@gmail.com>
2022-04-15 00:19:06 -0400

    deleted unnecessary link

--- ajaxForecast.js
+++ ajaxForecast.js
@@ -1,5 +1,4 @@
 function ajaxForecast(lat, lon) {
-  // https://api.openweathermap.org/data/2.5/forecast?q=London&cnt=5&appid=eff04efa5709d18068cb132cce23a366&units=metric
   // https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid==eff04efa5709d18068cb132cce23a366&units=metric
   var weather =
     "https://api.openweathermap.org/data/2.5/onecall?lat=" +
